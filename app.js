e441cdd1e8e612b91da6e966b3c588be70ee1db5
lmntapcode <120755103+lmntapcode@users.noreply.github.com>
2023-11-26 16:47:53 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', ()=>{
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll',()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundColor = 'linear-gradient(60deg, #0e0e0e 10%, #454545 90%';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
